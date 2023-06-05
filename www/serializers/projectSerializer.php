1c3fe659af08fd653d55e60efa981ee2052fa627
Félicien Corbat <felicien@champis.net>
2023-06-05 16:03:56 +0200

    Serialize project by id

--- www/serializers/projectSerializer.php
+++ www/serializers/projectSerializer.php
@@ -2,14 +2,20 @@
 
 use App\models\Project;
 
+require_once("../serializers/statusColumnSerializer.php");
+
 function serializeProjectById(Project $project): array
 {
-    dump($project);
     $status_columns = [];
     foreach ($project->getStatusColumns() as $statusColumn) {
-
+        $status_columns[] = serializeStatusColumn($statusColumn);
     }
-    return [];
+    return [
+        'id' => $project->getId(),
+        'title' => $project->getTitle(),
+        'status' => $project->getStatus(),
+        "status_columns" => $status_columns
+    ];
 }
 
 
--- www/serializers/statusColumnSerializer.php
+++ www/serializers/statusColumnSerializer.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\models\StatusColumn;
+
+require_once("../serializers/taskSerializer.php");
+
 function serializeStatusColumn(StatusColumn $statusColumn): array
 {
+    $tasks = [];
+    foreach ($statusColumn->getTasks() as $task) {
+        $tasks[] = serializeTask($task);
+    }
+
+    return [
+        'id' => $statusColumn->getId(),
+        'title' => $statusColumn->getTitle(),
+        'tasks' => $tasks
+    ];
 }
 
 
