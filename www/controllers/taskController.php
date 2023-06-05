9611dbda96d2be06ef9f77639d070540d7353bbf
24Syleo <sylvain.beggiora@outlook.com>
2023-06-05 09:46:43 +0200

    controller pas à jour

--- www/controllers/taskController.php
+++ www/controllers/taskController.php
@@ -4,6 +4,7 @@ require_once("../models/Task.php");
 require_once("../serializers/taskSerializer.php");
 require_once("./baseController.php");
 
+$task = new Task();
 
 function read(int $id)
 {
@@ -25,7 +26,13 @@ function create(stdClass $body): array
 
 function put(int $id, stdClass $body): array
 {
-    
+    try{
+        $result = $task->updateTask(int $id, $body);
+        return $result;
+    }
+    catch (ErrorException $e){
+        throw ($e);
+    }
 }
 
 function patch(int $id, stdClass $body): array
