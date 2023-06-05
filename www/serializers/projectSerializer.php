7f032812f2fb8d6cc4d11c7113805e0d18b92fd2
24Syleo <sylvain.beggiora@outlook.com>
2023-06-05 14:46:43 +0200

    control error

--- www/serializers/projectSerializer.php
+++ www/serializers/projectSerializer.php
@@ -21,7 +21,37 @@ function serializeProject(Project $project): array
 }
 
 
+/**
+ * deserializeProject
+ *
+ * @param  Project $body
+ * @return Project
+ */
 function deserializeProject(stdClass $body): Project
 {
+    $project = new Project();
+
+    if (!empty($body->title)) {
+        $project->setTitle($body->title);
+
+        if (strlen($body->title) > 250) {
+            throw new Exception("Le titre ne peut pas avoir plus que 100 caractères.", 400);
+        }
+    } else {
+        throw new Exception("Le titre ne peut pas être nul.", 400);
+    }
+
+    if (!empty($body->status)) {
+        $project->setTitle($body->status);
+    } else {
+        throw new Exception("Le status ne peut pas être nul.", 400);
+    }
+
+    if (!empty($body->copil_list)){
+        $project->setCopilList($body->copil_list);
+    } else {
+        throw new Exception(("La liste ne peut être vide"));
+    }
+
 }
 
