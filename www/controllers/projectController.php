38f51cb600425a8ccc77ff72f361f3fac4cafd52
24Syleo <sylvain.beggiora@outlook.com>
2023-06-07 13:33:39 +0200

    ok

--- www/controllers/projectController.php
+++ www/controllers/projectController.php
@@ -12,17 +12,6 @@ use OpenApi\Attributes as OA;
  * 
  * @throws Exception
  */
-#[OA\Get(
-    path:"/projects/id",
-    #[OA\Response(
-        response:"200",
-        description:"Kanban unique",
-        #[OA\JsonContent(
-            type:"string",
-            description:"titre du projet"
-        ),]
-    ),]
-)]
 function read(int $id)
 {
     $project = new Project();
