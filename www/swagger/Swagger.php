52a049c390d2caeec64c8807b26b38845b031296
24Syleo <sylvain.beggiora@outlook.com>
2023-06-07 15:07:57 +0200

    swagger json

--- www/swagger/Swagger.php
+++ www/swagger/Swagger.php
@@ -3,14 +3,16 @@ namespace App\swagger;
 
 require_once './vendor/autoload.php';
 
-use OpenApi\Attributes as OA;
+use OpenApi\Annotations as OA;
 
-#[OA\Info(
-    version:"1",
-    title:'API Kanban'
-)]
-#[OA\Server(
-    url:"http://localhost:8660",
-    description:"Kanban"
-)]
+/**
+ * @OA\Info(
+ *      title="API Kanban",
+ *      version="1"
+ *      )
+ * @OA\Server(
+ *      url="http://localhost:8660",
+ *      description="Notre Kanban"
+ *      )
+ */
 class Swagger {}
