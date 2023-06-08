0a233b798f120c5b4139e7f8bf63c0e3b79ef948
Yasm-ina <boumarafyasmina1991@gmail.com>
2023-06-08 10:02:38 +0200

    wip 3

--- www/models/Project.php
+++ www/models/Project.php
@@ -263,7 +263,7 @@ class Project extends Database
       }
    }
 
-   public function getUsersByProjectId(int $projectId): array
+   private function getUsersByProjectId(int $projectId): array
    {
       // get users (= copil list) from project
       $stmt = $this->pdo->prepare('SELECT user.id, user.last_name, user.first_name, user.image FROM project_user JOIN user ON project_user.user_id = user.id WHERE project_user.project_id = :project_id');
